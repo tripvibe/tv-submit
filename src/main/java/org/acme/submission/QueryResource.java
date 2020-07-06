2fb82d2cc2d3701b43f1e28d291cdd86919f0dfc
Mike Hepburn <eformat@gmail.com>
2020-07-06 15:31:41 +1000

    🦓 add more queries by id and add api doc 🦓

--- src/main/java/org/acme/submission/QueryResource.java
+++ src/main/java/org/acme/submission/QueryResource.java
@@ -1,6 +1,7 @@
 package org.acme.submission;
 
 import org.acme.data.Route;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,11 @@ public class QueryResource {
     @GET
     @Path("{route_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "getRoutes",
+            summary = "get submissions",
+            description = "This operation returns all submissions for a route_id",
+            deprecated = false,
+            hidden = false)
     public List<Route> getRoutes(@PathParam String route_id) {
         return entityManager.createQuery("select r from ROUTE" + route_id + " r", Route.class).getResultList();
     }
@@ -31,6 +37,11 @@ public class QueryResource {
     @GET
     @Path("/count/{route_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "count",
+            summary = "count submissions",
+            description = "This operation returns a count of all submissions for a route_id",
+            deprecated = false,
+            hidden = false)
     public Long count(@PathParam String route_id) {
         return entityManager.createQuery("select count(*) from ROUTE" + route_id + " r", Long.class).getSingleResult();
     }
@@ -38,6 +49,11 @@ public class QueryResource {
     @GET
     @Path("/count/{route_id}/{direction_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "countDirected",
+            summary = "query submissions by route and direction",
+            description = "This operation returns all submissions for a route_id and direction_id",
+            deprecated = false,
+            hidden = false)
     public Long countDirected(@PathParam String route_id, @PathParam String direction_id) {
         return entityManager.createQuery("select count(*) from ROUTE" + route_id + " r where direction_id = '\"" + direction_id + "\"'", Long.class).getSingleResult();
     }
@@ -45,6 +61,11 @@ public class QueryResource {
     @GET
     @Path("/capacity-average/{route_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgCapacity",
+            summary = "average capacity by route_id",
+            description = "This operation returns the average capacity by route_id",
+            deprecated = false,
+            hidden = false)
     public Double avgCapacity(@PathParam String route_id) {
         return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id, Double.class).getSingleResult();
     }
@@ -52,6 +73,11 @@ public class QueryResource {
     @GET
     @Path("/vibe-average/{route_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgVibe",
+            summary = "average vibe by route_id",
+            description = "This operation returns the average vibe by route_id",
+            deprecated = false,
+            hidden = false)
     public Double avgVibe(@PathParam String route_id) {
         return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id, Double.class).getSingleResult();
     }
@@ -59,6 +85,11 @@ public class QueryResource {
     @GET
     @Path("/capacity-average/{route_id}/{direction_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgCapacityDirected",
+            summary = "average capacity by route_id and direction_id",
+            description = "This operation returns the average capacity by route_id and direction_id",
+            deprecated = false,
+            hidden = false)
     public Double avgCapacityDirected(@PathParam String route_id, @PathParam String direction_id) {
         return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id + " where direction_id = '\"" + direction_id + "\"'", Double.class).getSingleResult();
     }
@@ -66,13 +97,23 @@ public class QueryResource {
     @GET
     @Path("/vibe-average/{route_id}/{direction_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgVibeDirected",
+            summary = "average vibe by route_id and direction_id",
+            description = "This operation returns the average vibe by route_id and direction_id",
+            deprecated = false,
+            hidden = false)
     public Double avgVibeDirected(@PathParam String route_id, @PathParam String direction_id) {
-        return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where route_direction = '\"" + direction_id + "\"'", Double.class).getSingleResult();
+        return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where direction_id = '\"" + direction_id + "\"'", Double.class).getSingleResult();
     }
 
     @GET
     @Path("/capacity-average/{route_id}/{start}/{end}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgCapacityDateTime",
+            summary = "average capacity by route_id between start and end datetime",
+            description = "This operation returns the average capacity by route_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
     public Double avgCapacityDateTime(@PathParam String route_id, @PathParam String start, @PathParam String end) {
         return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "'", Double.class).getSingleResult();
     }
@@ -80,22 +121,133 @@ public class QueryResource {
     @GET
     @Path("/vibe-average/{route_id}/{start}/{end}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgVibeDateTime",
+            summary = "average vibe by route_id between start and end datetime",
+            description = "This operation returns the average vibe by route_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
     public Double avgVibeDateTime(@PathParam String route_id, @PathParam String start, @PathParam String end) {
         return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "'", Double.class).getSingleResult();
     }
 
     @GET
+    @Path("/count/{route_id}/{start}/{end}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "countDateTime",
+            summary = "count of submissions by route_id between start and end datetime",
+            description = "This operation returns the count of submissions by route_id and between start and end datetime",
+            deprecated = false,
+            hidden = false)
+    public Double countDateTime(@PathParam String route_id, @PathParam String start, @PathParam String end) {
+        return entityManager.createQuery("select count(*) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "'", Double.class).getSingleResult();
+    }
+
+    @GET
     @Path("/capacity-average/{route_id}/{start}/{end}/{direction_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgCapacityDateTimeDirected",
+            summary = "average capacity by route_id, direction_id between start and end datetime",
+            description = "This operation returns the average capacity by route_id, direction_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
     public Double avgCapacityDateTimeDirected(@PathParam String route_id, @PathParam String start, @PathParam String end, @PathParam String direction_id) {
-        return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "' and route_direction = '\"" + direction_id + "\"'", Double.class).getSingleResult();
+        return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "' and direction_id = '\"" + direction_id + "\"'", Double.class).getSingleResult();
     }
 
     @GET
     @Path("/vibe-average/{route_id}/{start}/{end}/{direction_id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgVibeDateTimeDirected",
+            summary = "average vibe by route_id, direction_id between start and end datetime",
+            description = "This operation returns the average vibe by route_id, direction_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
     public Double avgVibeDateTimeDirected(@PathParam String route_id, @PathParam String start, @PathParam String end, @PathParam String direction_id) {
-        return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "' and route_direction = '\"" + direction_id + "\"'", Double.class).getSingleResult();
+        return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "' and direction_id = '\"" + direction_id + "\"'", Double.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/count/{route_id}/{start}/{end}/{direction_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "countDateTimeDirected",
+            summary = "count submissions by route_id, direction_id between start and end datetime",
+            description = "This operation returns the count of submissions by route_id, direction_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
+    public Long countDateTimeDirected(@PathParam String route_id, @PathParam String start, @PathParam String end, @PathParam String direction_id) {
+        return entityManager.createQuery("select count(*) from ROUTE" + route_id + " where departure_time between '" + start + "' and '" + end + "' and direction_id = '\"" + direction_id + "\"'", Long.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/capacity-average/{route_id}/{route_type}/{direction_id}/{run_id}/{stop_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgCapacityAllID",
+            summary = "average capacity by route_id, route_type, direction_id, run_id and stop_id",
+            description = "This operation returns the average capacity by route_id, route_type, direction_id, run_id and stop_id",
+            deprecated = false,
+            hidden = false)
+    public Double avgCapacityAllID(@PathParam String route_id, @PathParam String route_type, @PathParam String direction_id, @PathParam String run_id, @PathParam String stop_id) {
+        return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id + " where route_type = '\"" + route_type + "\"' and direction_id = '\"" + direction_id + "\"' and run_id = '\"" + run_id + "\"' and stop_id = '\"" + stop_id + "\"'", Double.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/vibe-average/{route_id}/{route_type}/{direction_id}/{run_id}/{stop_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgVibeAllID",
+            summary = "average vibe by route_id, route_type, direction_id, run_id and stop_id",
+            description = "This operation returns the average vibe by route_id, route_type, direction_id, run_id and stop_id",
+            deprecated = false,
+            hidden = false)
+    public Double avgVibeAllID(@PathParam String route_id, @PathParam String route_type, @PathParam String direction_id, @PathParam String run_id, @PathParam String stop_id) {
+        return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where route_type = '\"" + route_type + "\"' and direction_id = '\"" + direction_id + "\"' and run_id = '\"" + run_id + "\"' and stop_id = '\"" + stop_id + "\"'", Double.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/count/{route_id}/{route_type}/{direction_id}/{run_id}/{stop_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "countAllID",
+            summary = "count of submissions route_id, route_type, direction_id, run_id and stop_id",
+            description = "This operation returns the count of submissions by route_id, route_type, direction_id, run_id and stop_id",
+            deprecated = false,
+            hidden = false)
+    public Long countAllID(@PathParam String route_id, @PathParam String route_type, @PathParam String direction_id, @PathParam String run_id, @PathParam String stop_id) {
+        return entityManager.createQuery("select count(*) from ROUTE" + route_id + " where route_type = '\"" + route_type + "\"' and direction_id = '\"" + direction_id + "\"' and run_id = '\"" + run_id + "\"' and stop_id = '\"" + stop_id + "\"'", Long.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/capacity-average/{route_id}/{route_type}/{direction_id}/{run_id}/{stop_id}/{start}/{end}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgCapacityAllIDDatetime",
+            summary = "average capacity by route_id, route_type, direction_id and run_id between start and end datetime",
+            description = "This operation returns the average capacity by route_id, route_type, direction_id and run_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
+    public Double avgCapacityAllIDDatetime(@PathParam String route_id, @PathParam String route_type, @PathParam String direction_id, @PathParam String run_id, @PathParam String stop_id, @PathParam String start, @PathParam String end) {
+        return entityManager.createQuery("select avg(capacity) from ROUTE" + route_id + " where route_type = '\"" + route_type + "\"' and direction_id = '\"" + direction_id + "\"' and run_id = '\"" + run_id + "\"' and stop_id = '\"" + stop_id + "\"' and departure_time between '" + start + "' and '" + end + "'", Double.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/vibe-average/{route_id}/{route_type}/{direction_id}/{run_id}/{stop_id}/{start}/{end}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "avgVibeAllIDDatetime",
+            summary = "average vibe by route_id, route_type, direction_id and run_id between start and end datetime",
+            description = "This operation returns the average vibe by route_id, route_type, direction_id and run_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
+    public Double avgVibeAllIDDatetime(@PathParam String route_id, @PathParam String route_type, @PathParam String direction_id, @PathParam String run_id, @PathParam String stop_id, @PathParam String start, @PathParam String end) {
+        return entityManager.createQuery("select avg(vibe) from ROUTE" + route_id + " where route_type = '\"" + route_type + "\"' and direction_id = '\"" + direction_id + "\"' and run_id = '\"" + run_id + "\"' and stop_id = '\"" + stop_id + "\"' and departure_time between '" + start + "' and '" + end + "'", Double.class).getSingleResult();
+    }
+
+    @GET
+    @Path("/count/{route_id}/{route_type}/{direction_id}/{run_id}/{stop_id}/{start}/{end}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "countAllIDDatetime",
+            summary = "count of submissions by route_id, route_type, direction_id and run_id between start and end datetime",
+            description = "This operation returns the count of submissions by route_id, route_type, direction_id and run_id between start and end datetime",
+            deprecated = false,
+            hidden = false)
+    public Long countAllIDDatetime(@PathParam String route_id, @PathParam String route_type, @PathParam String direction_id, @PathParam String run_id, @PathParam String stop_id, @PathParam String start, @PathParam String end) {
+        return entityManager.createQuery("select count(*) from ROUTE" + route_id + " where route_type = '\"" + route_type + "\"' and direction_id = '\"" + direction_id + "\"' and run_id = '\"" + run_id + "\"' and stop_id = '\"" + stop_id + "\"' and departure_time between '" + start + "' and '" + end + "'", Long.class).getSingleResult();
     }
 
 }
